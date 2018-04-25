707817674f70a1b5a12a007d6e200870240588a2
George Drayson <drayson.g@mac.com>
2018-04-25 16:32:29 +0100

    Added fizzbuzz functionality, will refactor next

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,4 +1,11 @@
 class Fizzbuzz(object):
 
     def play(self, number):
-        return 'Fizz'
+        if number % 15 == 0:
+            return 'FizzBuzz'
+        elif number % 5 == 0:
+            return 'Buzz'
+        elif number % 3 == 0:
+            return 'Fizz'
+        else:
+            return number
--- src/test_fizzbuzz.py
+++ src/test_fizzbuzz.py
@@ -1,6 +1,42 @@
 import pytest
 from fizzbuzz import Fizzbuzz
 
-def test_returns_fizz_with_3():
-    fizzbuzz = Fizzbuzz()
-    assert fizzbuzz.play(3) == 'Fizz'
+def describe_multiples_3():
+
+    def test_returns_fizz_with_3():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(3) == 'Fizz'
+
+    def test_returns_fizz_with_6():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(6) == 'Fizz'
+
+def describe_multiples_5():
+
+    def test_returns_buzz_with_5():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(5) == 'Buzz'
+
+    def test_returns_buzz_with_10():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(10) == 'Buzz'
+
+def describe_multiples_15():
+
+    def test_returns_fizzbuzz_with_15():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(15) == 'FizzBuzz'
+
+    def test_returns_fizzbuzz_with_30():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(30) == 'FizzBuzz'
+
+def describe_normal_numbers():
+
+    def test_returns_4_with_4():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(4) == 4
+
+    def test_returns_7_with_7():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.play(7) == 7
