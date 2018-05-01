9f43c8f50947f84c11f14f4e550b5a33fd15bac7
George Drayson <drayson.g@mac.com>
2018-05-01 11:23:53 +0100

    added tests for is divisible by methods so that my tests are in isolation

--- lib/fizzbuzz.py
+++ lib/fizzbuzz.py
@@ -1,23 +1,29 @@
-class Fizzbuzz:
+class Fizzbuzz(object):
 
     def play(self, number):
-        if self.divisible_by_15(number):
+        if self.is_divisible_by_15(number):
             return 'FizzBuzz'
-        elif self.divisible_by_5(number):
+        elif self.is_divisible_by_5(number):
             return 'Buzz'
-        elif self.divisible_by_3(number):
+        elif self.is_divisible_by_3(number):
             return 'Fizz'
         else:
             return number
 
-    def divisible_by_15(self, number):
-        return self.divisible_by(15, number)
+    def is_divisible_by_15(self, number):
+        return self.is_divisible_by(15, number)
 
-    def divisible_by_5(self, number):
-        return self.divisible_by(5, number)
+    def is_divisible_by_5(self, number):
+        return self.is_divisible_by(5, number)
 
-    def divisible_by_3(self, number):
-        return self.divisible_by(3, number)
+    def is_divisible_by_3(self, number):
+        return self.is_divisible_by(3, number)
 
-    def divisible_by(self, divisor, number):
+    def is_divisible_by(self, divisor, number):
         return (number % divisor == 0)
+
+    def one_to_hundred(self):
+        list = []
+        for i in range(1,101):
+            list.append(self.play(i))
+        return ', '.join(str(x) for x in list)
--- src/test_fizzbuzz.py
+++ src/test_fizzbuzz.py
@@ -3,15 +3,57 @@ sys.path.insert(0, '/Users/georgedrayson/Projects/fizzbuzz_python/lib')
 
 from fizzbuzz import Fizzbuzz
 
+def describe_is_divisible_by():
+
+    def test_returns_true():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by(3,3) == True
+
+    def test_returns_false():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by(3,4) == False
+
+def describe_is_divisible_by_15():
+
+    def test_returns_true():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by_15(30) == True
+
+    def test_returns_false():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by_15(4) == False
+
+def describe_is_divisible_by_5():
+
+    def test_returns_true():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by_5(5) == True
+
+    def test_returns_false():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by_5(4) == False
+
+def describe_is_divisible_by_3():
+
+    def test_returns_true():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by_3(3) == True
+
+    def test_returns_false():
+        fizzbuzz = Fizzbuzz()
+        assert fizzbuzz.is_divisible_by_3(4) == False
+
 def describe_multiples_of_3():
 
     def test_returns_fizz_with_3():
         fizzbuzz = Fizzbuzz()
         assert fizzbuzz.play(3) == 'Fizz'
 
-    def test_returns_fizz_with_6():
+def describe_multiples_of_3():
+
+    def test_returns_fizz_with_3():
         fizzbuzz = Fizzbuzz()
-        assert fizzbuzz.play(6) == 'Fizz'
+        assert fizzbuzz.play(3) == 'Fizz'
 
 def describe_multiples_of_5():
 
@@ -19,19 +61,11 @@ def describe_multiples_of_5():
         fizzbuzz = Fizzbuzz()
         assert fizzbuzz.play(5) == 'Buzz'
 
-    def test_returns_buzz_with_10():
-        fizzbuzz = Fizzbuzz()
-        assert fizzbuzz.play(10) == 'Buzz'
-
 def describe_multiples_of_15():
 
     def test_returns_fizzbuzz_with_15():
         fizzbuzz = Fizzbuzz()
         assert fizzbuzz.play(15) == 'FizzBuzz'
-
-    def test_returns_fizzbuzz_with_30():
-        fizzbuzz = Fizzbuzz()
-        assert fizzbuzz.play(30) == 'FizzBuzz'
 
 def describe_normal_numbers():
 
@@ -39,6 +73,8 @@ def describe_normal_numbers():
         fizzbuzz = Fizzbuzz()
         assert fizzbuzz.play(4) == 4
 
-    def test_returns_7_with_7():
+def describe_one_to_100():
+
+    def test_returns_1_to_100():
         fizzbuzz = Fizzbuzz()
-        assert fizzbuzz.play(7) == 7
+        assert fizzbuzz.one_to_hundred() == '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, 31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, 41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, 61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, 71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, 91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, Buzz'
