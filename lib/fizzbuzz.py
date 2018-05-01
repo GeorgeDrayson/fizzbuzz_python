18a7bd4310a85a6aa3810a69d6862eb09bd4e84e
George Drayson <drayson.g@mac.com>
2018-05-01 10:44:02 +0100

    Added divisible_by_15 5 and 3 methods

--- lib/fizzbuzz.py
+++ lib/fizzbuzz.py
@@ -1,14 +1,23 @@
 class Fizzbuzz:
 
     def play(self, number):
-        if self.divisible_by(15, number):
+        if self.divisible_by_15(number):
             return 'FizzBuzz'
-        elif self.divisible_by(5, number):
+        elif self.divisible_by_5(number):
             return 'Buzz'
-        elif self.divisible_by(3, number):
+        elif self.divisible_by_3(number):
             return 'Fizz'
         else:
             return number
+
+    def divisible_by_15(self, number):
+        return self.divisible_by(15, number)
+
+    def divisible_by_5(self, number):
+        return self.divisible_by(5, number)
+
+    def divisible_by_3(self, number):
+        return self.divisible_by(3, number)
 
     def divisible_by(self, divisor, number):
         return (number % divisor == 0)
